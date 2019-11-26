b214333e3ffca9206909683d82a08ccc536a06e3
Mathieu Leplatre <mathieu@mozilla.com>
2019-11-26 15:43:54 +0100

    Fetch all collections records

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,67 @@ use serde_json;
 
 const SERVER_PROD: &'static str = "https://firefox.settings.services.mozilla.com/v1";
 
-#[derive(Debug, Deserialize)]
-struct PluralResponse {
-    data: serde_json::Value,
+type Record = serde_json::Value;
+
+#[derive(Deserialize)]
+struct RecordsResponse {
+    data: Vec<Record>,
+}
+
+struct RecordsListResult {
+    records: Vec<Record>,
+    timestamp: String,
 }
 
 async fn get_records(
-    server: String,
-    bucket: String,
-    collection: String,
-) -> Result<PluralResponse, reqwest::Error> {
+    server: &str,
+    bucket: &str,
+    collection: &str,
+) -> Result<RecordsListResult, reqwest::Error> {
     let url: String = format!(
         "{}/buckets/{}/collections/{}/records",
         server, bucket, collection
     );
-    println!("Fetch {:#?}...", url);
+    println!("Fetch {}...", url);
     let resp = reqwest::get(&url).await?;
+    let timestamp = resp
+        .headers()
+        .get("etag")
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
     let body = resp.text().await?;
-    let result: PluralResponse = serde_json::from_str(&body).unwrap();
-    Ok(result)
+
+    // Parse JSON response.
+    let result: RecordsResponse = serde_json::from_str(&body).unwrap();
+
+    Ok(RecordsListResult {
+        records: result.data,
+        timestamp: timestamp,
+    })
 }
 
 #[tokio::main]
 async fn main() {
-    let records = get_records(SERVER_PROD.into(), "main".into(), "cfr".into())
+    let result = get_records(SERVER_PROD, "monitor", "changes")
         .await
         .unwrap();
-    println!("{:#?}", records);
+
+    println!("Last modified {}", result.timestamp);
+
+    for entry in result.records {
+        let bucket = entry["bucket"].as_str().unwrap();
+        let collection = entry["collection"].as_str().unwrap();
+        if bucket.to_string().ends_with("preview") {
+            continue;
+        }
+        let col_result = get_records(SERVER_PROD, bucket, collection).await.unwrap();
+        println!(
+            "{}/{}: {} records.",
+            bucket,
+            collection,
+            col_result.records.len()
+        );
+    }
 }
